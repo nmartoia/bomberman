c2263462328a7d0428f2a03020e0438984a2bd64
nmartoia <nmartoia@edenschool.fr>
2022-06-21 09:34:32 +0200

    les personage ne peuve plus se rentrer dedans

--- js/app.js
+++ js/app.js
@@ -11,58 +11,59 @@ const perso2=document.createElement('img')
 perso2.src='img/perso2.png'
 perso2.alt='perso2'
 perso2.id='perso2'
+console.log(block[axeY+1].children[axeX].firstChild)
 window.addEventListener('keydown',(e)=>{
     if(e.code=='KeyS'){
-        if(axeY<4){
+        if(axeY<block.length-1&&block[axeY+1].children[axeX].firstChild==null){
             block[axeY].children[axeX].removeChild(block[axeY].children[axeX].firstChild)
             axeY++
             block[axeY].children[axeX].appendChild(perso)
         }
     }
     else if(e.code=='KeyW'){
-        if(axeY>0){
+        if(axeY>0&&block[axeY-1].children[axeX].firstChild==null){
             block[axeY].children[axeX].removeChild(block[axeY].children[axeX].firstChild)
             axeY--
             block[axeY].children[axeX].appendChild(perso)
         }
     }
     else if(e.code=='KeyA'){
-        if(axeX>0){
+        if(axeX>0&&block[axeY].children[axeX-1].firstChild==null){
             block[axeY].children[axeX].removeChild(block[axeY].children[axeX].firstChild)
             axeX--
             block[axeY].children[axeX].appendChild(perso)
         }
     }
     else if(e.code=='KeyD'){
-        if(axeX<8){
+        if(axeX<block[axeY].children.length-1&&block[axeY].children[axeX+1].firstChild==null){
             block[axeY].children[axeX].removeChild(block[axeY].children[axeX].firstChild)
             axeX++
             block[axeY].children[axeX].appendChild(perso)
         }
     }
     else if(e.code=='ArrowDown'){
-        if(axeY1<4){
+        if(axeY1<block.length-1&&block[axeY1+1].children[axeX1].firstChild==null){
             block[axeY1].children[axeX1].removeChild(block[axeY1].children[axeX1].firstChild)
             axeY1++
             block[axeY1].children[axeX1].appendChild(perso2)
         }    
     }
     else if(e.code=='ArrowUp'){
-        if(axeY1>0){
+        if(axeY1>0&&block[axeY1-1].children[axeX1].firstChild==null){
             block[axeY1].children[axeX1].removeChild(block[axeY1].children[axeX1].firstChild)
             axeY1--
             block[axeY1].children[axeX1].appendChild(perso2)
         }
     }   
     else if(e.code=='ArrowLeft'){
-        if(axeX1>0){
+        if(axeX1>0&&block[axeY1].children[axeX1-1].firstChild==null){
             block[axeY1].children[axeX1].removeChild(block[axeY1].children[axeX1].firstChild)
             axeX1--
             block[axeY1].children[axeX1].appendChild(perso2)
         }
     }
     else if(e.code=='ArrowRight'){
-        if(axeX1<8){
+        if(axeX1<block[axeY1].children.length-1&&block[axeY1].children[axeX1+1].firstChild==null){
             block[axeY1].children[axeX1].removeChild(block[axeY1].children[axeX1].firstChild)
             axeX1++
             block[axeY1].children[axeX1].appendChild(perso2)
